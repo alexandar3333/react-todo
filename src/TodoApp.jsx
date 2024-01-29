ba1f9db85fe8b34a1308913868e2dfe20d9ef9a4
Alexander Hristov <aleksandar.hristov@forkpoint.com>
2024-01-29 13:08:51 +0200

    initial commit

---
+++ src/TodoApp.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './assets/sass/App.scss'
+
+function TodoApp() {
+
+  return (
+    <div className="main">
+      <h1>TODO LIST</h1>
+      
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </div>
+  )
+}
+
+export default TodoApp
